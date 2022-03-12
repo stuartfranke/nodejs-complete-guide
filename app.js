1390c33a63c9ffe018003c59c90016ed09636dde
Stuart Franke <sf@vectra.co.za>
2022-03-12 13:06:51 +0200

    Section 6 - Assignment

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const path = require('path');
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const users = [];
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.get("/", (req, res, next) => {
+  res.render("index", { pageTitle: "Add User" });
+});
+
+app.get("/users", (req, res, next) => {
+  res.render("users", { pageTitle: "Users", users: users });
+});
+
+app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect("/users");
 });
 
 app.listen(3000);
